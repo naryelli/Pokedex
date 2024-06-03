1bf8e3257434fccf7f4c3bb9f1e8c7cb1cff603f
naryelli <naryelli_henrique@estudante.sesisenai.org.br>
2024-06-03 12:25:46 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
 import './App.css'
 import Pokedex from './components/Pokedex'
+import React, {useState, useEffect} from 'react'
+import style from 'styled-components'
+import{motion} from "framer-motion"
+
+const Spinner = style (motion.div)`
+ width: 50px;
+ height:50px;
+ border: 4px solid black;
+ border-top: 4px solid red;
+ border-radius: 50%;
+ display:incline-block;
+`;
+
+const Conteiner = style.div`
+display: flex;
+width:100vw;
+align-items: center;
+justify-content: center;
+height:100vw;
+`
 
 function App() {
- 
+  const[carregando, setCarregando] = useState(true);
+
+  useEffect(() => {
+    const tempo = setTimeout(()=>{
+      setCarregando(false);
+    }, 5000)
+    return ()=> clearTimeout(tempo);
+  }, [])
 
   return (
-    <Pokedex />
-    
+    <>
+      <Conteiner>
+        {carregando ? (
+           <Spinner
+           animate={{rotate: 360}}
+           transition={{duration: 1, repeat: Infinity, ease: 'linear'}}
+           ></Spinner>
+        ):(
+          <Pokedex></Pokedex>
+        )}
+
+      </Conteiner>
+      
+    </>
   )
 }
 
